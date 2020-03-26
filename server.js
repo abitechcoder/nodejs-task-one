45fcebe6c421ba30135c322aeebf26ccc7b313f2
abitechcoder <abiolaolalekan39@gmail.com>
2020-03-26 18:47:42 +0100

    added comment to the code

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+// included the http, filesystem and querystring module
 const http = require('http');
 const fs = require('fs');
 var qs = require('querystring');
 
+// created the server
 const server = http.createServer(function(req, res){
+    // reading the index.html with the filesystem module fs
     fs.readFile('index.html', function(err, data){
         res.writeHead(200, {'Content-Type': 'text/html'});
+    // sending the data to the browser after reading
         res.write(data);
+    // checks if the request method is a post method
         if (req.method == 'POST') {
+    // creates a body variable to store the string format of the html form
             var body = '';
             req.on('data', function (data) {
                 body += data;
@@ -17,9 +23,10 @@ const server = http.createServer(function(req, res){
                 }
             });
             req.on('end', function () {
+                // using the querystring to parse the body string into the created Post variable
                 const POST = qs.parse(body);
-                // use POST
-                //console.log(POST.message)
+                // writing the value of the input named message into the file message.txt
+                // using the filesystem method writefile
                 fs.writeFile('message.txt', `${POST.message}`, function (err) {
                     if (err) throw err;
                     console.log('File Saved!');
